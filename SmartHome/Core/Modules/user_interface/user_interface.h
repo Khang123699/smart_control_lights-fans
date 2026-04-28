c32de5cc44d5f972a0226bc84e47d998a013337a
KHANG <khangnbk2004@gmail.com>
2026-04-28 11:18:59 +0700

    Add user interface with OLED SSD1306 support

---
+++ SmartHome/Core/Modules/user_interface/user_interface.h
@@ -0,0 +1,14 @@
+/*
+ * user_interface.h
+ *
+ *  Created on: May 20, 2026
+ *      Author: Admin
+ */
+
+#ifndef _USER_INTERFACE_H_
+#define _USER_INTERFACE_H_
+
+void userInterfaceInit(void);
+void userInterfaceUpdate(void);
+
+#endif /* _USER_INTERFACE_H_ */
